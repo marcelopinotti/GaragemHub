eb8b0680b1b301f3103dfaea44cf0b136fa1e198
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-08 20:29:45 -0300

    refactor: remove unused imports in CarrosModel and GlobalExceptionHandler

--- src/main/java/marcelo/HeroGarage/exception/GlobalExceptionHandler.java
+++ src/main/java/marcelo/HeroGarage/exception/GlobalExceptionHandler.java
@@ -3,10 +3,8 @@ package marcelo.HeroGarage.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
