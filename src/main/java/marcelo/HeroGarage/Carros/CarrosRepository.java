6c2056702071ad63c10d091fa42261fa1e154ffd
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-02-10 14:11:06 -0300

    feat: add method to find cars by personagemId in CarrosRepository

--- src/main/java/marcelo/HeroGarage/Carros/CarrosRepository.java
+++ src/main/java/marcelo/HeroGarage/Carros/CarrosRepository.java
@@ -2,5 +2,8 @@ package marcelo.HeroGarage.Carros;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarrosRepository extends JpaRepository<CarrosModel,Long> {
+    List<CarrosModel> findByPersonagemId(Long personagemId);
 }
